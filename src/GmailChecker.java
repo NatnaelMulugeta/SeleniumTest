7340b7bd6c2ee81041fe40b5500cac41eb18d621
Natnael Mulugeta <natnaelmu.07@gmail.com>
2019-06-22 16:14:51 +1100

    Selenium Test Demo

---
+++ src/GmailChecker.java
@@ -0,0 +1,52 @@
+package autotest;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import java.io.PrintWriter;
+import java.util.List;
+
+public class GmailChecker {
+
+    public static void main(String[] args) throws Exception {
+        System.setProperty("webdriver.chrome.driver" , "/home/natnaelmulugeta/chromedriver");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--no-sandbox");
+
+        WebDriver driver = new ChromeDriver(options);
+        String appUrl = "http://mail.google.com";
+        driver.get(appUrl);
+
+
+        WebElement emailInput = driver.findElement(By.id("identifierId"));
+        WebElement emailNextButton = driver.findElement(By.id("identifierNext"));
+        emailInput.sendKeys("email");
+        emailNextButton.click();
+
+        Thread.sleep(1000);
+        WebElement passwordInput = driver.findElement(By.xpath("//*[@id=\"password\"]/div[1]/div/div[1]/input"));
+        WebElement passwordNextutton = driver.findElement(By.id("passwordNext"));
+        passwordInput.sendKeys("password");
+        passwordNextutton.click();
+
+        Thread.sleep(2000);
+
+        List<WebElement> unreadEmails = driver.findElements(By.xpath("//*[@class='zA zE']"));
+
+        System.out.println(unreadEmails.size());
+        PrintWriter writer = new PrintWriter("emails.txt", "UTF-8");
+        for(int i = 0; i < unreadEmails.size(); i++) {
+            WebElement from = unreadEmails.get(i).findElement(By.xpath(".//*[@class='yW']"));
+            writer.println("From : " + from.getText());
+
+            WebElement subject = unreadEmails.get(i).findElement(By.xpath(".//*[@class='bog']"));
+            writer.println("Subject : " + subject.getText());
+
+        }
+        //writer.println("end");
+        writer.close();
+
+    }
+}
---
+++ src/GradeReport.java
@@ -0,0 +1,35 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import java.io.FileWriter;
+
+
+public class GradeReport {
+    public static void main(String[] args){
+        String id= "username";
+        String password ="password";
+        System.setProperty("webdriver.chrome.driver", "/home/natnaelmulugeta/chromedriver");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--no-sandbox");
+
+        WebDriver driver = new ChromeDriver(options);
+        String appUrl = "http://portal.aait.edu.et";
+        driver.get(appUrl);
+
+        driver.findElement(By.xpath("//*[@id=\"UserName\"]")).sendKeys(id);
+        driver.findElement(By.xpath("//*[@id=\"Password\"]")).sendKeys(password);
+        driver.findElement(By.xpath("//*[@id=\"home\"]/div[2]/div[2]/form/div[4]/div/button\n")).submit();
+        driver.get("https://portal.aait.edu.et/Grade/GradeReport\n");
+
+        String grade = driver.findElement(By.xpath("/html/body/div[2]/div/div[2]/div[1]/div")).getText();
+
+        try{
+            FileWriter fw=new FileWriter("gradeReport.txt");
+            fw.write(grade);
+            fw.close();
+        }catch(Exception e){
+            System.out.println(e);
+        }
+    }
+}
